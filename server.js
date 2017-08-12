fa0e1b20ca55bc9b933d3c3b6fcc418d7208ca38
Caroline Bowes <carvbowes@gmail.com>
2017-08-12 11:16:47 -0400

    routes

--- server.js
+++ server.js
@@ -12,6 +12,55 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.text());
 app.use(bodyParser.json({type: "application/vnd.api+json"}));
 
+var reservations = [{
+	name: "yoda",
+	number: "9999999999",
+	email: "yoda@theforce.org",
+	id: "sizemattersnot"
+}, {
+	name: "aang",
+	number: "0101010101",
+	email: "thelastairbender@avatar.com",
+	id: "momowashere"
+}, {
+	name: "cuzco",
+	number: "3333333333",
+	email: "theemperor@peru.gov",
+	id: "imperialhairness"
+}];
+
+var waitlist = [{
+	name: "",
+	number: "",
+	email: "",
+	id: ""
+}];
+
+//routes
+app.get("/", function(req, res) {
+	res.sendFile(path.join(__dirname, "home.html"));
+});
+
+app.get("/tables", function(req, res) {
+	res.sendFile(path.join(__dirname, "tables.html"));
+});
+
+app.get("/reserve", function(req, res) {
+	res.sendFile(path.join(__dirname, "reserve.html"));
+});
+
+app.get("/api/tables", function(req, res) {
+	res.json(reservations);
+});
+
+app.get("/api/waitlist", function(req, res) {
+	res.json(waitlist);
+});
+
+//reservation create
+
+//server start
 app.listen(PORT, function() {
 	console.log("app listening on port " + PORT);
-})
+});
+
